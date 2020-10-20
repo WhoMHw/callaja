9c6ea19afb1e7b814c86467712b833a8cc8c5719
WhoMHw <71681090+WhoMHw@users.noreply.github.com>
2020-10-20 22:49:43 +0700

    Add files via upload

---
+++ wibi.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+clear
+
+echo "═══════════════════════════════════════════════════════════════════════════════════════"
+echo "            _Author :WhoMHw_							    "
+echo "				_tool milik bersama_					    "
+echo "══════════════════════════════════════════════════════════════════════════════════════"
+
+ echo "selamat datang ditools ini"
+echo "siapa nama kaka?" #tulisan keluar
+read nama #membaca
+clear
+ echo "thank " $nama
+termux-open https://wa.me/+6285336515046
+echo "telah berkunjung"
+sleep 2
+
+    echo "?"
+read -p "Masukkan nomor target : " nomor
+link="https://id.jagreward.com/member/verify-mobile/$nomor"
+curl -s $link
+else
+echo "terimakasih" 
+exit
+fi
+echo
+
